edc471998200a33af1d53a072ec144850bc66d5c
Ben <redraskal@users.noreply.github.com>
2017-06-21 21:25:09 -0500

    Dead End will now (probably) load with a map

--- src/main/java/me/redraskal/arcadia/Arcadia.java
+++ src/main/java/me/redraskal/arcadia/Arcadia.java
@@ -1,9 +1,13 @@
 package me.redraskal.arcadia;
 
+import me.redraskal.arcadia.api.game.BaseGame;
+import me.redraskal.arcadia.api.map.GameMap;
 import me.redraskal.arcadia.game.DeadEndGame;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.List;
+import java.util.Random;
 
 public class Arcadia extends JavaPlugin {
 
@@ -19,6 +23,25 @@ public class Arcadia extends JavaPlugin {
         final File mapFolder = new File(this.getDataFolder().getPath() + "/maps/");
         mapFolder.mkdirs();
         this.getAPI().getMapRegistry().loadMaps(mapFolder);
+
+        // Custom rotation soontm
+        for(Class<? extends BaseGame> game
+                : this.getAPI().getGameRegistry().getRegisteredGames()) {
+            this.getAPI().getGameManager().getRotation().addGame(game);
+        }
+
+        this.nextGameInRotation(true);
+    }
+
+    /**
+     * Switches to the next game in the rotation!
+     */
+    public void nextGameInRotation(boolean first) {
+        if(!first) this.getAPI().getGameManager().getRotation().nextGame();
+        List<GameMap> possibleMaps = this.getAPI().getGameRegistry().getMaps(this.getAPI().getGameManager().getRotation().getCurrentGame());
+        this.getAPI().getGameManager().setCurrentGame(
+                this.getAPI().getGameManager().getRotation().getCurrentGame(),
+                possibleMaps.get(new Random().nextInt(possibleMaps.size())));
     }
 
     /**
