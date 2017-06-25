3c4eb424e08eb0bda3ea50cd8ce1e7b326e41413
Ben <redraskal@users.noreply.github.com>
2017-06-25 12:44:07 -0500

    Added glass to RedLightGreenLight game

--- src/main/java/me/redraskal/arcadia/game/RedLightGreenLightGame.java
+++ src/main/java/me/redraskal/arcadia/game/RedLightGreenLightGame.java
@@ -1,5 +1,6 @@
 package me.redraskal.arcadia.game;
 
+import me.redraskal.arcadia.Cuboid;
 import me.redraskal.arcadia.Utils;
 import me.redraskal.arcadia.api.game.BaseGame;
 import me.redraskal.arcadia.api.map.GameMap;
@@ -9,18 +10,23 @@ import me.redraskal.arcadia.api.scoreboard.defaults.RelativeDistanceSidebar;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.Iterator;
 
 public class RedLightGreenLightGame extends BaseGame {
 
     private Location startPosition;
     private Location targetPosition;
     private String towards;
+    private Cuboid glass;
 
     public RedLightGreenLightGame(GameMap gameMap) {
-        super("Red Light, Green Light", new String[]{"startPosition", "targetPosition", "targetTowards"},
+        super("Red Light, Green Light", new String[]{"startPosition", "targetPosition", "targetTowards", "glassBoundsA", "glassBoundsB"},
                 new SidebarSettings(RelativeDistanceSidebar.class,
                     WinMethod.HIGHEST_SCORE, 1, 30), gameMap,
                 "Run when the light is green. Stop when the light is red.");
@@ -37,10 +43,22 @@ public class RedLightGreenLightGame extends BaseGame {
         this.targetPosition = Utils.parseLocation((String) this.getGameMap().fetchSetting("targetPosition"));
         this.towards = (String) this.getGameMap().fetchSetting("targetTowards");
         ((RelativeDistanceSidebar) this.getSidebar()).setTarget(targetPosition, towards);
+        this.glass = new Cuboid(Utils.parseLocation((String) this.getGameMap().fetchSetting("glassBoundsA")),
+                Utils.parseLocation((String) this.getGameMap().fetchSetting("glassBoundsB")));
+        Iterator<Block> glassBlocks = glass.iterator();
+        while(glassBlocks.hasNext()) {
+            glassBlocks.next().setType(Material.GLASS);
+        }
     }
 
     @Override
-    public void onGameStart() {}
+    public void onGameStart() {
+        Iterator<Block> glassBlocks = glass.iterator();
+        while(glassBlocks.hasNext()) {
+            glassBlocks.next().setType(Material.AIR);
+        }
+        //TODO: Fun runnable system
+    }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
